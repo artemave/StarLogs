457eb7bf1ec23f07cdc74eb38d639681e3c2dcc4
artemave <artemave@gmail.com>
2013-04-13 18:04:28 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 if [ "$1" = "--all" ] ; then
   s3cmd put -r * s3://starlogs.net
 else
-  s3cmd put **/*.{html,css,js} s3://starlogs.net
+  s3cmd put starlogs.html s3://starlogs.net
+  s3cmd put javascripts/guts.js s3://starlogs.net/javascripts/guts.js
+  s3cmd put stylesheets/styles.css s3://starlogs.net/stylesheets/styles.css
 fi
